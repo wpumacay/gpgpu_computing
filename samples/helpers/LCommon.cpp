b3431a8791b72ccb3ba3cc19cb15f16d03f676b3
wpumacay <wpumacay@gmail.com>
2017-09-20 12:59:54 -0500

    fixed an issue

--- samples/helpers/LCommon.cpp
+++ samples/helpers/LCommon.cpp
@@ -7,7 +7,7 @@ namespace common
 {
 
 
-	void printDeviceProps( int deviceId = -1 )
+	void printDeviceProps( int deviceId )
 	{
 		if ( deviceId == -1 )
 		{
@@ -30,8 +30,8 @@ namespace common
 			cout << "Name: " << _props.name;
 			cout << "Compute capability: " << _props.major << "." << _props.minor << endl;
 			cout << "Clock rate: " << _props.clockRate << endl;
-			cout << "Device copy overlap: " << ( _props.deviceOverlap ) ? "Enabled" : "Disabled" << endl;
-			cout << "Kernel execution timeout: " << ( _props.kernelExecTimeoutEnabled ) ? "Enabled" : "Disabled" << endl;
+			cout << "Device copy overlap: " << ( ( _props.deviceOverlap ) ? "Enabled" : "Disabled" ) << endl;
+			cout << "Kernel execution timeout: " << ( ( _props.kernelExecTimeoutEnabled ) ? "Enabled" : "Disabled" ) << endl;
 
 			cout << " --- Memory information for device " << deviceId << " ---" << endl;
 			cout << "Total global memory: " << _props.totalGlobalMem << endl;
@@ -41,7 +41,7 @@ namespace common
 
 			cout << " --- Multiprocessor information for device " << deviceId << " ---" << endl;
 			cout << "Num multiprocessors: " << _props.multiProcessorCount << endl;
-			cout << "Shared memory per multiprocessor: " << _props.shaderMemPerBlock << endl;
+			cout << "Shared memory per multiprocessor: " << _props.sharedMemPerBlock << endl;
 			cout << "Registers per multiprocessor: " << _props.regsPerBlock << endl;
 			cout << "Threads in warp: " << _props.warpSize << endl;
 
